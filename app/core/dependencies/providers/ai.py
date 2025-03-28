f4277af2860e8898d4b809e860291596182dcdf6
mickey-semy <toshkin.mikhail@yandex.ru>
2025-03-28 03:52:31 +0000

    fix: провайдер для ai

--- app/core/dependencies/providers/ai.py
+++ app/core/dependencies/providers/ai.py
@@ -2,10 +2,12 @@ from dishka import Provider, Scope, provide
 from redis import Redis
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.core.integrations.cache.ai import AIRedisStorage
 from app.services.v1.modules.ai.service import AIService
 
 
 class AIProvider(Provider):
     @provide(scope=Scope.REQUEST)
     def ai_service(self, db_session: AsyncSession, redis: Redis) -> AIService:
-        return AIService(db_session, redis)
+        redis_storage = AIRedisStorage(redis)
+        return AIService(db_session, redis_storage)
