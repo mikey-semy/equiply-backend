855d5c0f444f26f05136b451aa9487eb1f5925b9
Mike <34812500+mikey-semy@users.noreply.github.com>
2025-05-04 22:50:17 +0300

    fix: исправление ошибки в базовой модели

--- app/models/v1/base.py
+++ app/models/v1/base.py
@@ -51,6 +51,7 @@ class BaseModel(DeclarativeBase):
 
     metadata = MetaData()
 
+    @classmethod
     def table_name(cls) -> str:
         """
         Возвращает имя таблицы, на которую ссылается модель.
@@ -61,6 +62,7 @@ class BaseModel(DeclarativeBase):
 
         return cls.__tablename__
 
+    @classmethod
     def fields(cls: Type[T]) -> List[str]:
         """
         Возвращает список имен полей модели.
