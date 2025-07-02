8508652e32c739c9dc0472d56afbc1af443e6d79
Mike <34812500+mikey-semy@users.noreply.github.com>
2025-07-02 12:13:53 +0300

    Update vk.py

--- app/services/v1/oauth/providers/vk.py
+++ app/services/v1/oauth/providers/vk.py
@@ -12,7 +12,7 @@ from app.schemas import (OAuthProvider, VKOAuthParamsSchema,
                          VKUserDataSchema)
 from app.services.v1.auth.service import AuthService
 from app.services.v1.oauth.base import BaseOAuthProvider
-from app.services.v1.register.service import RegisterService
+from app.services.v1.registration.service import RegisterService
 from app.services.v1.users.service import UserService
 
 from ..data_manager import OAuthDataManager
